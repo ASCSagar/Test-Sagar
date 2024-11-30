5204691cc7d644399096df1b3ade4d52b8262190
ASCSagar <ascsagar08@gmail.com>
2024-11-30 12:32:01 +0530

    change the  main.yml

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Test from "./component/Test";
 import Sagar from "./component/Sagar";
 import Ramani from "./component/Ramani";
+import Ishwarbhai from "./component/Ishwarbhai";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Test />} />
+        <Route path="/" element={<Ishwarbhai />} />
         <Route path="/sagar" element={<Sagar />} />
         <Route path="/ramani" element={<Ramani />} />
       </Routes>
